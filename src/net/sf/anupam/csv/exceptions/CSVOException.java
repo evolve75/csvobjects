c1dcdd82f1a9fe7dcc8610275adbb80d22fe2303
Anupam Sengupta <anupamsg@users.sourceforge.net>
2005-08-05 04:14:44 +0000

    Fixed a synchronization issue with the singletons, which was causing problems with parsing of XML configurations.

---
+++ src/net/sf/anupam/csv/exceptions/CSVOException.java
@@ -0,0 +1,67 @@
+/*
+ * CSVOException.java
+ *
+ * Copyright (C) 2005 Anupam Sengupta (anupamsg@users.sourceforge.net)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * Version: $Revision$
+ */
+package net.sf.anupam.csv.exceptions;
+
+/**
+ * A checked exception for CSV Objects framework. This exception is used internally
+ * within the framework.
+ *
+ * @author Anupam Sengupta
+ * @version $Revision$
+ */
+public class CSVOException extends Exception {
+
+    /**
+     * Default Constructor.
+     */
+    public CSVOException() {
+        super();
+    }
+
+    /**
+     * Constructor accepting a message string.
+     *
+     * @param message the error message
+     */
+    public CSVOException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Constructor accepting a message and a root cause exception to embed.
+     *
+     * @param message the error message
+     * @param cause   the root cause exception
+     */
+    public CSVOException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructor accepting the root cause exception to embed.
+     *
+     * @param cause the root cause exception
+     */
+    public CSVOException(final Throwable cause) {
+        super(cause);
+    }
+}
